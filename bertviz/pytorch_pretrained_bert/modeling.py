fd88b19c667c7b2784ea74ecf5093fc16f36b383
Bayartsogt <bayartsogtyadamsuren@icloud.com>
2019-06-20 08:58:17 +0800

    unnecessary codes while reading fine-tuned tf checkpoints

--- bertviz/pytorch_pretrained_bert/modeling.py
+++ bertviz/pytorch_pretrained_bert/modeling.py
@@ -58,8 +58,8 @@ def load_tf_weights_in_bert(model, tf_checkpoint_path):
     """ Load tf checkpoints in a pytorch model
     """
     try:
-        import re
-        import numpy as np
+        # import re
+        # import numpy as np
         import tensorflow as tf
     except ImportError:
         print("Loading a TensorFlow models in PyTorch, requires TensorFlow to be installed. Please see "
@@ -69,49 +69,49 @@ def load_tf_weights_in_bert(model, tf_checkpoint_path):
     print("Converting TensorFlow checkpoint from {}".format(tf_path))
     # Load weights from TF model
     init_vars = tf.train.list_variables(tf_path)
-    names = []
-    arrays = []
-    for name, shape in init_vars:
-        print("Loading TF weight {} with shape {}".format(name, shape))
-        array = tf.train.load_variable(tf_path, name)
-        names.append(name)
-        arrays.append(array)
+    # names = []
+    # arrays = []
+    # for name, shape in init_vars:
+    #     print("Loading TF weight {} with shape {}".format(name, shape))
+    #     array = tf.train.load_variable(tf_path, name)
+    #     names.append(name)
+    #     arrays.append(array)
 
-    for name, array in zip(names, arrays):
-        name = name.split('/')
-        # adam_v and adam_m are variables used in AdamWeightDecayOptimizer to calculated m and v
-        # which are not required for using pretrained model
-        if any(n in ["adam_v", "adam_m"] for n in name):
-            print("Skipping {}".format("/".join(name)))
-            continue
-        pointer = model
-        for m_name in name:
-            if re.fullmatch(r'[A-Za-z]+_\d+', m_name):
-                l = re.split(r'_(\d+)', m_name)
-            else:
-                l = [m_name]
-            if l[0] == 'kernel' or l[0] == 'gamma':
-                pointer = getattr(pointer, 'weight')
-            elif l[0] == 'output_bias' or l[0] == 'beta':
-                pointer = getattr(pointer, 'bias')
-            elif l[0] == 'output_weights':
-                pointer = getattr(pointer, 'weight')
-            else:
-                pointer = getattr(pointer, l[0])
-            if len(l) >= 2:
-                num = int(l[1])
-                pointer = pointer[num]
-        if m_name[-11:] == '_embeddings':
-            pointer = getattr(pointer, 'weight')
-        elif m_name == 'kernel':
-            array = np.transpose(array)
-        try:
-            assert pointer.shape == array.shape
-        except AssertionError as e:
-            e.args += (pointer.shape, array.shape)
-            raise
-        print("Initialize PyTorch weight {}".format(name))
-        pointer.data = torch.from_numpy(array)
+    # for name, array in zip(names, arrays):
+    #     name = name.split('/')
+    #     # adam_v and adam_m are variables used in AdamWeightDecayOptimizer to calculated m and v
+    #     # which are not required for using pretrained model
+    #     if any(n in ["adam_v", "adam_m"] for n in name):
+    #         print("Skipping {}".format("/".join(name)))
+    #         continue
+    #     pointer = model
+    #     for m_name in name:
+    #         if re.fullmatch(r'[A-Za-z]+_\d+', m_name):
+    #             l = re.split(r'_(\d+)', m_name)
+    #         else:
+    #             l = [m_name]
+    #         if l[0] == 'kernel' or l[0] == 'gamma':
+    #             pointer = getattr(pointer, 'weight')
+    #         elif l[0] == 'output_bias' or l[0] == 'beta':
+    #             pointer = getattr(pointer, 'bias')
+    #         elif l[0] == 'output_weights':
+    #             pointer = getattr(pointer, 'weight')
+    #         else:
+    #             pointer = getattr(pointer, l[0])
+    #         if len(l) >= 2:
+    #             num = int(l[1])
+    #             pointer = pointer[num]
+    #     if m_name[-11:] == '_embeddings':
+    #         pointer = getattr(pointer, 'weight')
+    #     elif m_name == 'kernel':
+    #         array = np.transpose(array)
+    #     try:
+    #         assert pointer.shape == array.shape
+    #     except AssertionError as e:
+    #         e.args += (pointer.shape, array.shape)
+    #         raise
+    #     print("Initialize PyTorch weight {}".format(name))
+    #     pointer.data = torch.from_numpy(array)
     return model
 
 
