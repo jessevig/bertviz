7e733d781502234a94a54fbb244ad925f85029df
jessevig <jesse.vig@gmail.com>
2019-07-10 17:30:04 -0700

    Added new javascript files.

---
+++ bertviz/head_view.js
@@ -0,0 +1,361 @@
+/**
+ * @fileoverview Transformer Visualization D3 javascript code.
+ *
+ *
+
+ *  Based on: https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/visualization/attention.js
+ *
+ * Change log:
+ *
+ * 12/19/18  Jesse Vig   Assorted cleanup. Changed orientation of attention matrices.
+ */
+
+requirejs(['jquery', 'd3'], function($, d3) {
+
+const TEXT_SIZE = 15;
+const BOXWIDTH = TEXT_SIZE * 8;
+const BOXHEIGHT = TEXT_SIZE * 1.5;
+const WIDTH = 2000;
+const HEIGHT_PADDING = 100;
+const MATRIX_WIDTH = 150;
+const HEAD_COLORS = d3.scale.category10();
+const CHECKBOX_SIZE = 20;
+
+var params = window.params;
+var config = {};
+
+function lighten(color) {
+  var c = d3.hsl(color);
+  var increment = (1 - c.l) * 0.6;
+  c.l += increment;
+  c.s -= increment;
+  return c;
+}
+
+function transpose(mat) {
+  return mat[0].map(function(col, i) {
+    return mat.map(function(row) {
+      return row[i];
+    });
+  });
+}
+
+function zip(a, b) {
+  return a.map(function (e, i) {
+    return [e, b[i]];
+  });
+}
+
+function render() {
+
+  var attnData = config.attention[config.filter];
+  var leftText = attnData.left_text;
+  var rightText = attnData.right_text;
+  var attentionHeads = attnData.attn[config.layer];
+
+  $("#vis svg").empty();
+  $("#vis").empty();
+
+  var height = config.initialTextLength * BOXHEIGHT * 2 + HEIGHT_PADDING;
+  var svg = d3.select("#vis")
+            .append('svg')
+            .attr("width", WIDTH)
+            .attr("height", height);
+
+  var attData = [];
+  for (var i=0; i < config.nHeads; i++) {
+    var att = attentionHeads[i];
+    var att_trans = transpose(att);
+    attData.push(zip(att_trans, att));
+  }
+
+  renderText(svg, leftText, true, attData, 0);
+  renderText(svg, rightText, false, attData, MATRIX_WIDTH + BOXWIDTH);
+
+  renderAttentionHighlights(svg, attData);
+
+  svg.append("g").classed("attentionHeads", true);
+
+  renderAttention(svg, attentionHeads);
+
+  drawCheckboxes(0, svg, attentionHeads);
+
+}
+
+function renderText(svg, text, isLeft, attData, leftPos) {
+  // attData: list of tuples (att, att_trans), one for each layer. att and att_trans are attention matrics for each layer.
+  //           att is of shape [nHeads, source_len, target_len)
+  var id = isLeft ? "left" : "right";
+  var textContainer = svg.append("svg:g")
+                         .attr("id", id);
+
+  textContainer.append("g").classed("attentionBoxes", true)
+               .selectAll("g")
+               .data(attData)
+               .enter()
+               .append("g")
+               .selectAll("rect")
+               .data(function(d) {return d;})
+               .enter()
+               .append("rect")
+               .attr("x", function(d, i, j) {
+                 return leftPos + boxOffsets(j);
+               })
+               .attr("y", function(d, i) {
+                 return (+1) * BOXHEIGHT;
+               })
+               .attr("width", BOXWIDTH / activeHeads())
+               .attr("height", function() { return BOXHEIGHT; })
+               .attr("fill", function(d, i, j) {
+                  return HEAD_COLORS(j);
+                })
+               .style("opacity", 0.0);
+
+  var tokenContainer = textContainer.append("g").selectAll("g")
+                                    .data(text)
+                                    .enter()
+                                    .append("g");
+
+  tokenContainer.append("rect")
+                .classed("background", true)
+                .style("opacity", 0.0)
+                .attr("fill", "lightgray")
+                .attr("x", leftPos)
+                .attr("y", function(d, i) {
+                  return (i+1) * BOXHEIGHT;
+                })
+                .attr("width", BOXWIDTH)
+                .attr("height", BOXHEIGHT);
+
+  var textEl = tokenContainer.append("text")
+                              .text(function(d) { return d; })
+                              .attr("font-size", TEXT_SIZE + "px")
+                              .style("cursor", "default")
+                              .style("-webkit-user-select", "none")
+                              .attr("x", leftPos)
+                              .attr("y", function(d, i) {
+                                return (i+1) * BOXHEIGHT;
+                              });
+
+  if (isLeft) {
+    textEl.style("text-anchor", "end")
+           .attr("dx", BOXWIDTH - TEXT_SIZE)
+           .attr("dy", TEXT_SIZE);
+  } else {
+    textEl.style("text-anchor", "start")
+           .attr("dx", + TEXT_SIZE)
+           .attr("dy", TEXT_SIZE);
+  }
+
+  tokenContainer.on("mouseover", function(d, index) {
+    textContainer.selectAll(".background")
+                 .style("opacity", function(d, i) {
+                   return i == index ? 1.0 : 0.0;
+                 });
+
+    svg.selectAll(".attentionHeads").style("display", "none");
+
+    svg.selectAll(".lineHeads")  // To get the nesting to work.
+       .selectAll(".attLines")
+       .attr("stroke-opacity", function(d) {
+          return 1.0;
+        })
+       .attr("y1", function(d, i) {
+        if (isLeft) {
+          return (index+1) * BOXHEIGHT + (BOXHEIGHT/2);
+        } else {
+          return (i+1) * BOXHEIGHT + (BOXHEIGHT/2);
+        }
+     })
+     .attr("x1", BOXWIDTH)
+     .attr("y2", function(d, i) {
+       if (isLeft) {
+          return (i+1) * BOXHEIGHT + (BOXHEIGHT/2);
+        } else {
+          return (index+1) * BOXHEIGHT + (BOXHEIGHT/2);
+        }
+     })
+     .attr("x2", BOXWIDTH + MATRIX_WIDTH)
+     .attr("stroke-width", 2)
+     .attr("stroke", function(d, i, j) {
+        return HEAD_COLORS(j);
+      })
+     .attr("stroke-opacity", function(d, i, j) {
+      if (isLeft) {d = d[0];} else {d = d[1];}
+      if (config.headVis[j]) {
+        if (d) {
+          return d[index];
+        } else {
+          return 0.0;
+        }
+      } else {
+        return 0.0;
+      }
+     });
+
+    function updateAttentionBoxes() {
+      var id = isLeft ? "right" : "left";
+      var leftPos = isLeft ? MATRIX_WIDTH + BOXWIDTH : 0;
+      svg.select("#" + id)
+         .selectAll(".attentionBoxes")
+         .selectAll("g")
+         .selectAll("rect")
+         .attr("x", function(d, i, j) { return leftPos + boxOffsets(j); })
+         .attr("y", function(d, i) { return (i+1) * BOXHEIGHT; })
+         .attr("width", BOXWIDTH/activeHeads())
+         .attr("height", function() { return BOXHEIGHT; })
+         .style("opacity", function(d, i, j) {
+            if (isLeft) {d = d[0];} else {d = d[1];}
+            if (config.headVis[j])
+              if (d) {
+                return d[index];
+              } else {
+                return 0.0;
+              }
+            else
+              return 0.0;
+         });
+    }
+
+    updateAttentionBoxes();
+  });
+
+  textContainer.on("mouseleave", function() {
+    d3.select(this).selectAll(".background")
+                   .style("opacity", 0.0);
+    svg.selectAll(".attLines").attr("stroke-opacity", 0.0);
+    svg.selectAll(".attentionHeads").style("display", "inline");
+    svg.selectAll(".attentionBoxes")
+       .selectAll("g")
+       .selectAll("rect")
+       .style("opacity", 0.0);
+  });
+}
+
+function renderAttentionHighlights(svg, attention) {
+  var line_container = svg.append("g");
+  line_container.selectAll("g")
+                .data(attention)
+                .enter()
+                .append("g")
+                .classed("lineHeads", true)
+                .selectAll("line")
+                .data(function(d){return d;})
+                .enter()
+                .append("line").classed("attLines", true);
+}
+
+function renderAttention(svg, attentionHeads) {
+  var line_container = svg.selectAll(".attentionHeads");
+  line_container.html(null);
+  for(var h=0; h<attentionHeads.length; h++) {
+    for(var s=0; s<attentionHeads[h].length; s++) {
+      for(var a=0; a<attentionHeads[h][s].length; a++) {
+        line_container.append("line")
+        .attr("y1", (s+1) * BOXHEIGHT + (BOXHEIGHT/2))
+        .attr("x1", BOXWIDTH)
+        .attr("y2", (a+1) * BOXHEIGHT + (BOXHEIGHT/2))
+        .attr("x2", BOXWIDTH + MATRIX_WIDTH)
+        .attr("stroke-width", 2)
+        .attr("stroke", HEAD_COLORS(h))
+        .attr("stroke-opacity", function() {
+          if (config.headVis[h]) {
+            return attentionHeads[h][s][a]/activeHeads();
+          } else {
+            return 0.0;
+          }
+        }());
+      }
+    }
+  }
+}
+
+// Checkboxes
+function boxOffsets(i) {
+  var numHeadsAbove = config.headVis.reduce(
+      function(acc, val, cur) {return val && cur < i ? acc + 1: acc;}, 0);
+  return numHeadsAbove * (BOXWIDTH / activeHeads());
+}
+
+function activeHeads() {
+  return config.headVis.reduce(function(acc, val) {
+    return val ? acc + 1: acc;
+  }, 0);
+}
+
+function drawCheckboxes(top, svg, attentionHeads) {
+  var checkboxContainer = svg.append("g");
+  var checkbox = checkboxContainer.selectAll("rect")
+                                  .data(config.headVis)
+                                  .enter()
+                                  .append("rect")
+                                  .attr("fill", function(d, i) {
+                                    return HEAD_COLORS(i);
+                                  })
+                                  .attr("x", function(d, i) {
+                                    return (i+1) * CHECKBOX_SIZE;
+                                  })
+                                  .attr("y", top)
+                                  .attr("width", CHECKBOX_SIZE)
+                                  .attr("height", CHECKBOX_SIZE);
+
+  function updateCheckboxes() {
+    checkboxContainer.selectAll("rect")
+                              .data(config.headVis)
+                              .attr("fill", function(d, i) {
+      var headColor = HEAD_COLORS(i);
+      var color = d ? headColor : lighten(headColor);
+      return color;
+    });
+  }
+
+  updateCheckboxes();
+
+  checkbox.on("click", function(d, i) {
+    if (config.headVis[i] && activeHeads() == 1) return;
+    config.headVis[i] = !config.headVis[i];
+    updateCheckboxes();
+    renderAttention(svg, attentionHeads);
+  });
+
+  checkbox.on("dblclick", function(d, i) {
+    // If we double click on the only active head then reset
+    if (config.headVis[i] && activeHeads() == 1) {
+      config.headVis = new Array(config.nHeads).fill(true);
+    } else {
+      config.headVis = new Array(config.nHeads).fill(false);
+      config.headVis[i] = true;
+    }
+    updateCheckboxes();
+    renderAttention(svg, attentionHeads);
+  });
+}
+
+function initialize() {
+  config.attention = params['attention'];
+  config.filter = params['default_filter'];
+  config.nHeads = config.attention[config.filter]['attn'][0].length;
+  config.headVis  = new Array(config.nHeads).fill(true);
+  config.layer = 0;
+  config.initialTextLength = config.attention[config.filter].right_text.length
+}
+
+$("#layer").empty();
+for(var i=0; i<12; i++) {
+  $("#layer").append($("<option />").val(i).text(i));
+}
+
+$("#layer").on('change', function(e) {
+  config.layer = +e.currentTarget.value;
+  render();
+});
+
+$("#filter").on('change', function(e) {
+  config.filter = e.currentTarget.value;
+  render();
+});
+
+initialize();
+render();
+
+});
---
+++ bertviz/model_view.js
@@ -0,0 +1,326 @@
+/**
+ * @fileoverview Transformer Visualization D3 javascript code.
+ *
+ * Based on: https://github.com/tensorflow/tensor2tensor/blob/master/tensor2tensor/visualization/attention.js
+ *
+ * Change log:
+ *
+ * 02/01/19  Jesse Vig   Initial implementation
+ */
+
+
+requirejs(['jquery', 'd3'], function($, d3) {
+
+        var params = window.params;
+        var config = {};
+
+        const MIN_X = 0;
+        const MIN_Y = 0;
+
+        const THUMBNAIL_WIDTH = 79;
+        const THUMBNAIL_BOX_HEIGHT = 7;
+        const THUMBNAIL_ATTENTION_WIDTH = 65;
+        const THUMBNAIL_PADDING = 5;
+
+        const DETAIL_WIDTH = 300;
+        const DETAIL_ATTENTION_WIDTH = 140;
+        const DETAIL_BOX_WIDTH = 80;
+        const DETAIL_BOX_HEIGHT = 20;
+        const DETAIL_PADDING = 5;
+        const DETAIL_HEADING_HEIGHT = 47;
+        const DETAIL_HEADING_TEXT_SIZE = 17;
+        const TEXT_SIZE = 13;
+
+        const LAYER_COLORS = d3.scaleOrdinal(d3.schemeCategory10);
+
+        function render() {
+
+            // Set global state variables
+
+            var attData = config.attention[config.filter];
+            config.leftText = attData.left_text;
+            config.rightText = attData.right_text;
+            config.attn = attData.attn;
+            config.numLayers = config.attn.length;
+            config.numHeads = config.attn[0].length;
+            config.thumbnailHeight = Math.max(config.leftText.length, config.rightText.length) * THUMBNAIL_BOX_HEIGHT + 2 * THUMBNAIL_PADDING;
+            config.detailHeight = Math.max(config.leftText.length, config.rightText.length) * DETAIL_BOX_HEIGHT + 2 * DETAIL_PADDING + DETAIL_HEADING_HEIGHT;
+            config.divHeight = config.numLayers * config.thumbnailHeight;
+            config.divWidth = config.numHeads * THUMBNAIL_WIDTH;
+
+            $("#vis").empty();
+            config.svg = d3.select("#vis")
+                .append('svg')
+                .attr("width", config.divWidth)
+                .attr("height", config.divHeight)
+              .attr("fill", "black");
+
+            var i;
+            var j;
+            for (i = 0; i < config.numLayers; i++) {
+                for (j = 0; j < config.numHeads; j++) {
+                    renderThumbnail(i, j);
+                }
+            }
+        }
+
+        function renderThumbnail(layerIndex, headIndex) {
+            var x = headIndex * THUMBNAIL_WIDTH;
+            var y = layerIndex * config.thumbnailHeight;
+            renderThumbnailAttn(x, y, config.attn[layerIndex][headIndex], layerIndex, headIndex);
+        }
+
+        function renderDetail(att, layerIndex, headIndex) {
+            var xOffset = 64;
+            var maxX = config.divWidth;
+            var maxY = config.divHeight;
+            var x = headIndex * THUMBNAIL_WIDTH + THUMBNAIL_PADDING + xOffset;
+            if (x < MIN_X) {
+                x = MIN_X;
+            } else if (x + DETAIL_WIDTH > maxX) {
+                x = headIndex * THUMBNAIL_WIDTH + THUMBNAIL_PADDING - DETAIL_WIDTH + 8;
+            }
+            var posLeftText = x;
+            var posAttention = posLeftText + DETAIL_BOX_WIDTH;
+            var posRightText = posAttention + DETAIL_ATTENTION_WIDTH;
+            var thumbnailHeight = Math.max(config.leftText.length, config.rightText.length) * THUMBNAIL_BOX_HEIGHT + 2 * THUMBNAIL_PADDING;
+            var yOffset = 20;
+            var y = layerIndex * thumbnailHeight + THUMBNAIL_PADDING + yOffset;
+            if (y < MIN_Y) {
+                y = MIN_Y;
+            } else if (y + config.detailHeight > maxY) {
+                y = maxY - config.detailHeight;
+            }
+            renderDetailFrame(x, y, layerIndex);
+            renderDetailHeading(x, y, layerIndex, headIndex);
+            renderText(config.leftText, "leftText", posLeftText, y + DETAIL_HEADING_HEIGHT, layerIndex);
+            renderDetailAttn(posAttention, y + DETAIL_HEADING_HEIGHT, att, layerIndex, headIndex);
+            renderText(config.rightText, "rightText", posRightText, y + DETAIL_HEADING_HEIGHT, layerIndex);
+        }
+
+        function renderDetailHeading(x, y, layerIndex, headIndex) {
+            var fillColor = LAYER_COLORS(layerIndex % 10);
+            config.svg.append("text")
+                .classed("detail", true)
+                .text('Layer ' + layerIndex + ", Head " + headIndex)
+                .attr("font-size", DETAIL_HEADING_TEXT_SIZE + "px")
+                .style("cursor", "default")
+                .style("-webkit-user-select", "none")
+                .attr("fill", fillColor)
+                .attr("x", x + 88)
+                .attr("y", y + 16)
+                .attr("height", DETAIL_HEADING_HEIGHT)
+                .attr("width", DETAIL_WIDTH)
+                .attr("dy", DETAIL_HEADING_TEXT_SIZE);
+        }
+
+        function renderText(text, id, x, y, layerIndex) {
+            var tokenContainer = config.svg.append("svg:g")
+                .classed("detail", true)
+                .selectAll("g")
+                .data(text)
+                .enter()
+                .append("g");
+
+            var fillColor = LAYER_COLORS(layerIndex % 10);
+
+            tokenContainer.append("rect")
+                .classed("highlight", true)
+                .attr("fill", fillColor)
+                .style("opacity", 0.0)
+                .attr("height", DETAIL_BOX_HEIGHT)
+                .attr("width", DETAIL_BOX_WIDTH)
+                .attr("x", x)
+                .attr("y", function (d, i) {
+                    return y + i * DETAIL_BOX_HEIGHT - 1;
+                });
+
+            var textContainer = tokenContainer.append("text")
+                .classed("token", true)
+                .text(function (d) {
+                    return d;
+                })
+                .attr("font-size", TEXT_SIZE + "px")
+                .style("cursor", "default")
+                .style("-webkit-user-select", "none")
+                .attr("fill", fillColor)
+                .attr("x", x)
+                .attr("y", function (d, i) {
+                    return i * DETAIL_BOX_HEIGHT + y;
+                })
+                .attr("height", DETAIL_BOX_HEIGHT)
+                .attr("width", DETAIL_BOX_WIDTH)
+                .attr("dy", TEXT_SIZE);
+
+            if (id == "leftText") {
+                textContainer.style("text-anchor", "end")
+                    .attr("dx", DETAIL_BOX_WIDTH - 2);
+                tokenContainer.on("mouseover", function (d, index) {
+                    highlightSelection(index);
+                });
+                tokenContainer.on("mouseleave", function () {
+                    unhighlightSelection();
+                });
+            }
+        }
+
+        function highlightSelection(index) {
+            config.svg.select("#leftText")
+                .selectAll(".highlight")
+                .style("opacity", function (d, i) {
+                    return i == index ? 1.0 : 0.0;
+                });
+            config.svg.selectAll(".attn-line-group")
+                .style("opacity", function (d, i) {
+                    return i == index ? 1.0 : 0.0;
+                });
+        }
+
+        function unhighlightSelection() {
+            config.svg.select("#leftText")
+                .selectAll(".highlight")
+                .style("opacity", 0.0);
+            config.svg.selectAll(".attn-line-group")
+                .style("opacity", 1);
+        }
+
+        function renderThumbnailAttn(x, y, att, layerIndex, headIndex) {
+
+            var attnContainer = config.svg.append("svg:g");
+
+            var attnBackground = attnContainer.append("rect")
+                .attr("id", 'attn_background_' + layerIndex + "_" + headIndex)
+                .classed("attn_background", true)
+                .attr("x", x)
+                .attr("y", y)
+                .attr("height", config.thumbnailHeight)
+                .attr("width", THUMBNAIL_WIDTH)
+                .attr("stroke-width", 2)
+                .attr("stroke", LAYER_COLORS(layerIndex % 10))
+                .attr("stroke-opacity", 0);
+            var x1 = x + THUMBNAIL_PADDING;
+            var x2 = x1 + THUMBNAIL_ATTENTION_WIDTH;
+            var y1 = y + THUMBNAIL_PADDING;
+
+            attnContainer.selectAll("g")
+                .data(att)
+                .enter()
+                .append("g") // Add group for each source token
+                .attr("source-index", function (d, i) { // Save index of source token
+                    return i;
+                })
+                .selectAll("line")
+                .data(function (d) { // Loop over all target tokens
+                    return d;
+                })
+                .enter() // When entering
+                .append("line")
+                .attr("x1", x1)
+                .attr("y1", function (d) {
+                    var sourceIndex = +this.parentNode.getAttribute("source-index");
+                    return y1 + (sourceIndex + .5) * THUMBNAIL_BOX_HEIGHT;
+                })
+                .attr("x2", x2)
+                .attr("y2", function (d, targetIndex) {
+                    return y1 + (targetIndex + .5) * THUMBNAIL_BOX_HEIGHT;
+                })
+                .attr("stroke-width", 3)
+                .attr("stroke", LAYER_COLORS(layerIndex % 10))
+                .attr("stroke-opacity", function (d) {
+                    return d;
+                });
+
+            var clickRegion = attnContainer.append("rect")
+                .attr("x", x)
+                .attr("y", y)
+                .attr("height", config.thumbnailHeight)
+                .attr("width", THUMBNAIL_WIDTH)
+                .style("opacity", 0);
+
+            clickRegion.on("click", function (d, index) {
+                var attnBackgroundOther = config.svg.selectAll(".attn_background");
+                attnBackgroundOther.attr("fill", "black");
+                attnBackgroundOther.attr("stroke-opacity", 0);
+
+                config.svg.selectAll(".detail").remove();
+                if (config.detail_layer != layerIndex || config.detail_head != headIndex) {
+                    renderDetail(att, layerIndex, headIndex);
+                    config.detail_layer = layerIndex;
+                    config.detail_head = headIndex;
+                    attnBackground.attr("fill", "#202020");
+                    attnBackground.attr("stroke-opacity", .8);
+                } else {
+                    config.detail_layer = null;
+                    config.detail_head = null;
+                    attnBackground.attr("fill", "black");
+                    attnBackground.attr("stroke-opacity", 0);
+                }
+            });
+
+            clickRegion.on("mouseover", function (d) {
+                d3.select(this).style("cursor", "pointer");
+            });
+        }
+
+        function renderDetailFrame(x, y, layerIndex) {
+            var detailFrame = config.svg.append("rect")
+                .classed("detail", true)
+                .attr("x", x)
+                .attr("y", y)
+                .attr("height", config.detailHeight)
+                .attr("width", DETAIL_WIDTH)
+                .style("opacity", 1)
+                .attr("fill", "white")
+                .attr("stroke-width", 2)
+                .attr("stroke-opacity", 0.7)
+                .attr("stroke", LAYER_COLORS(layerIndex % 10));
+        }
+
+        function renderDetailAttn(x, y, att, layerIndex) {
+            var attnContainer = config.svg.append("svg:g")
+                .classed("detail", true)
+                .attr("pointer-events", "none");
+            attnContainer.selectAll("g")
+                .data(att)
+                .enter()
+                .append("g") // Add group for each source token
+                .classed('attn-line-group', true)
+                .attr("source-index", function (d, i) { // Save index of source token
+                    return i;
+                })
+                .selectAll("line")
+                .data(function (d) { // Loop over all target tokens
+                    return d;
+                })
+                .enter()
+                .append("line")
+                .attr("x1", x + DETAIL_PADDING)
+                .attr("y1", function (d) {
+                    var sourceIndex = +this.parentNode.getAttribute("source-index");
+                    return y + (sourceIndex + .5) * DETAIL_BOX_HEIGHT;
+                })
+                .attr("x2", x + DETAIL_ATTENTION_WIDTH - DETAIL_PADDING)
+                .attr("y2", function (d, targetIndex) {
+                    return y + (targetIndex + .5) * DETAIL_BOX_HEIGHT;
+                })
+                .attr("stroke-width", 2)
+                .attr("stroke", LAYER_COLORS(layerIndex % 10))
+                .attr("stroke-opacity", function (d) {
+                    return d;
+                });
+        }
+
+        function initialize() {
+          config.attention = params['attention'];
+          config.filter = params['default_filter'];
+        }
+
+        $("#filter").on('change', function (e) {
+            config.filter = e.currentTarget.value;
+            render();
+        });
+
+        initialize();
+        render();
+
+    });
