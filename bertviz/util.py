108bc0265aeb623f8fa0edd77d5471b5f534dcab
Jesse V <jesse.vig@gmail.com>
2020-03-28 06:11:34 -0700

    Added check that attention tensor has correct number of dimensions.

--- bertviz/util.py
+++ bertviz/util.py
@@ -3,7 +3,10 @@ import torch
 def format_attention(attention):
     squeezed = []
     for layer_attention in attention:
-        # num_heads x seq_len x seq_len
+        # 1 x num_heads x seq_len x seq_len
+        if len(layer_attention.shape) != 4:
+            raise ValueError("The attention tensor does not have the correct number of dimensions. Make sure you set "
+                             "output_attentions=True when initializing your model.")
         squeezed.append(layer_attention.squeeze(0))
     # num_layers x num_heads x seq_len x seq_len
     return torch.stack(squeezed)
