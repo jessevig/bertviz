0cf0aa54cb172037911655901df79ec687b7502f
Tuco-Code <danisantiagoco@gmail.com>
2022-03-23 12:17:54 +0100

    Update requirement URLs for d3 and jQuery (source of bug not allowing end result to be displayed on browsers)

--- bertviz/head_view.js
+++ bertviz/head_view.js
@@ -12,12 +12,13 @@
  * 02/06/21  Jesse Vig   Move require config from separate jupyter notebook step
  * 05/03/21  Jesse Vig   Adjust height of visualization dynamically
  * 07/25/21  Jesse Vig   Support layer filtering
+ * 23/03/22  Daniel SC   Update requirement URLs for d3 and jQuery (source of bug not allowing end result to be displayed on browsers)
  **/
 
 require.config({
   paths: {
-      d3: '//cdnjs.cloudflare.com/ajax/libs/d3/5.7.0/d3.min',
-    jquery: '//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min',
+      d3: 'https://cdnjs.cloudflare.com/ajax/libs/d3/5.7.0/d3.min',
+    jquery: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.0.0/jquery.min',
   }
 });
 
